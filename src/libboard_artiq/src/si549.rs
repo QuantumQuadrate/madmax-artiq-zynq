3b0b52ef2c31cb0213d16062ca70322a1bf530dc
Simon Renblad <srenblad@m-labs.hk>
2025-11-10 13:41:52 +0800

    si549: clean up remaining GlobalTimer usage
    Missing as it did not show up in testing / searching.

--- src/libboard_artiq/src/si549.rs
+++ src/libboard_artiq/src/si549.rs
@@ -81,15 +81,15 @@ mod i2c {
         sda_oe(dcxo, false);
 
         // Check the I2C bus is ready
-        half_period(timer);
-        half_period(timer);
+        half_period();
+        half_period();
         if !sda_i(dcxo) {
             // Try toggling SCL a few times
             for _bit in 0..8 {
                 scl_o(dcxo, false);
-                half_period(timer);
+                half_period();
                 scl_o(dcxo, true);
-                half_period(timer);
+                half_period();
             }
         }
 
@@ -102,21 +102,21 @@ mod i2c {
     pub fn start(dcxo: DCXO) {
         // Set SCL high then SDA low
         scl_o(dcxo, true);
-        half_period(timer);
+        half_period();
         sda_oe(dcxo, true);
-        half_period(timer);
+        half_period();
     }
 
     pub fn stop(dcxo: DCXO) {
         // First, make sure SCL is low, so that the target releases the SDA line
         scl_o(dcxo, false);
-        half_period(timer);
+        half_period();
         // Set SCL high then SDA high
         sda_oe(dcxo, true);
         scl_o(dcxo, true);
-        half_period(timer);
+        half_period();
         sda_oe(dcxo, false);
-        half_period(timer);
+        half_period();
     }
 
     pub fn write(dcxo: DCXO, data: u8) -> bool {
@@ -125,19 +125,19 @@ mod i2c {
             // Set SCL low and set our bit on SDA
             scl_o(dcxo, false);
             sda_oe(dcxo, data & (1 << bit) == 0);
-            half_period(timer);
+            half_period();
             // Set SCL high ; data is shifted on the rising edge of SCL
             scl_o(dcxo, true);
-            half_period(timer);
+            half_period();
         }
         // Check ack
         // Set SCL low, then release SDA so that the I2C target can respond
         scl_o(dcxo, false);
-        half_period(timer);
+        half_period();
         sda_oe(dcxo, false);
         // Set SCL high and check for ack
         scl_o(dcxo, true);
-        half_period(timer);
+        half_period();
         // returns true if acked (I2C target pulled SDA low)
         !sda_i(dcxo)
     }
@@ -146,7 +146,7 @@ mod i2c {
         // Set SCL low first, otherwise setting SDA as input may cause a transition
         // on SDA with SCL high which will be interpreted as START/STOP condition.
         scl_o(dcxo, false);
-        half_period(timer); // make sure SCL has settled low
+        half_period(); // make sure SCL has settled low
         sda_oe(dcxo, false);
 
         let mut data: u8 = 0;
@@ -154,10 +154,10 @@ mod i2c {
         // MSB first
         for bit in (0..8).rev() {
             scl_o(dcxo, false);
-            half_period(timer);
+            half_period();
             // Set SCL high and shift data
             scl_o(dcxo, true);
-            half_period(timer);
+            half_period();
             if sda_i(dcxo) {
                 data |= 1 << bit
             }
@@ -168,78 +168,78 @@ mod i2c {
         if ack {
             sda_oe(dcxo, true)
         }
-        half_period(timer);
+        half_period();
         // then set SCL high
         scl_o(dcxo, true);
-        half_period(timer);
+        half_period();
 
         data
     }
 }
 
 fn write(dcxo: i2c::DCXO, reg: u8, val: u8) -> Result<(), &'static str> {
-    i2c::start(dcxo, timer);
-    if !i2c::write(dcxo, ADDRESS << 1, timer) {
+    i2c::start(dcxo);
+    if !i2c::write(dcxo, ADDRESS << 1) {
         return Err("Si549 failed to ack write address");
     }
-    if !i2c::write(dcxo, reg, timer) {
+    if !i2c::write(dcxo, reg) {
         return Err("Si549 failed to ack register");
     }
-    if !i2c::write(dcxo, val, timer) {
+    if !i2c::write(dcxo, val) {
         return Err("Si549 failed to ack value");
     }
-    i2c::stop(dcxo, timer);
+    i2c::stop(dcxo);
     Ok(())
 }
 
 fn read(dcxo: i2c::DCXO, reg: u8) -> Result<u8, &'static str> {
-    i2c::start(dcxo, timer);
-    if !i2c::write(dcxo, ADDRESS << 1, timer) {
+    i2c::start(dcxo);
+    if !i2c::write(dcxo, ADDRESS << 1) {
         return Err("Si549 failed to ack write address");
     }
-    if !i2c::write(dcxo, reg, timer) {
+    if !i2c::write(dcxo, reg) {
         return Err("Si549 failed to ack register");
     }
-    i2c::stop(dcxo, timer);
+    i2c::stop(dcxo);
 
-    i2c::start(dcxo, timer);
-    if !i2c::write(dcxo, (ADDRESS << 1) | 1, timer) {
+    i2c::start(dcxo);
+    if !i2c::write(dcxo, (ADDRESS << 1) | 1) {
         return Err("Si549 failed to ack read address");
     }
-    let val = i2c::read(dcxo, false, timer);
-    i2c::stop(dcxo, timer);
+    let val = i2c::read(dcxo, false);
+    i2c::stop(dcxo);
     Ok(val)
 }
 
 fn setup(dcxo: i2c::DCXO, config: &DividerConfig) -> Result<(), &'static str> {
-    i2c::init(dcxo, timer)?;
+    i2c::init(dcxo)?;
 
-    write(dcxo, 255, 0x00, timer)?; // PAGE
-    write(dcxo, 69, 0x00, timer)?; // Disable FCAL override.
-    write(dcxo, 17, 0x00, timer)?; // Synchronously disable output
+    write(dcxo, 255, 0x00)?; // PAGE
+    write(dcxo, 69, 0x00)?; // Disable FCAL override.
+    write(dcxo, 17, 0x00)?; // Synchronously disable output
 
     // The Si549 has no ID register, so we check that it responds correctly
     // by writing values to a RAM-like register and reading them back.
     for test_value in 0..255 {
-        write(dcxo, 23, test_value, timer)?;
-        let readback = read(dcxo, 23, timer)?;
+        write(dcxo, 23, test_value)?;
+        let readback = read(dcxo, 23)?;
         if readback != test_value {
             return Err("Si549 detection failed");
         }
     }
 
-    write(dcxo, 23, config.hsdiv as u8, timer)?;
-    write(dcxo, 24, (config.hsdiv >> 8) as u8 | (config.lsdiv << 4), timer)?;
-    write(dcxo, 26, config.fbdiv as u8, timer)?;
-    write(dcxo, 27, (config.fbdiv >> 8) as u8, timer)?;
-    write(dcxo, 28, (config.fbdiv >> 16) as u8, timer)?;
-    write(dcxo, 29, (config.fbdiv >> 24) as u8, timer)?;
-    write(dcxo, 30, (config.fbdiv >> 32) as u8, timer)?;
-    write(dcxo, 31, (config.fbdiv >> 40) as u8, timer)?;
+    write(dcxo, 23, config.hsdiv as u8)?;
+    write(dcxo, 24, (config.hsdiv >> 8) as u8 | (config.lsdiv << 4))?;
+    write(dcxo, 26, config.fbdiv as u8)?;
+    write(dcxo, 27, (config.fbdiv >> 8) as u8)?;
+    write(dcxo, 28, (config.fbdiv >> 16) as u8)?;
+    write(dcxo, 29, (config.fbdiv >> 24) as u8)?;
+    write(dcxo, 30, (config.fbdiv >> 32) as u8)?;
+    write(dcxo, 31, (config.fbdiv >> 40) as u8)?;
 
-    write(dcxo, 7, 0x08, timer)?; // Start FCAL
-    timer.delay_us(30_000); // Internal FCAL VCO calibration
-    write(dcxo, 17, 0x01, timer)?; // Synchronously enable output
+    write(dcxo, 7, 0x08)?; // Start FCAL
+    timer::delay_us(30_000); // Internal FCAL VCO calibration
+    write(dcxo, 17, 0x01)?; // Synchronously enable output
 
     Ok(())
 }
