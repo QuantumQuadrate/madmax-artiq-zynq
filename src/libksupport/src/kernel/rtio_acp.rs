72407a19a365730f3ec4f08132fca69d2c6216a2
mwojcik <mw@m-labs.hk>
2025-11-21 14:37:29 +0800

    rtio_acp: decrease latency for rtio events move the status clear to after the status is read, rather than beginning this saves the time for slow volatile write fixes test_exceptions RTIO underflow test

--- src/libksupport/src/kernel/rtio_acp.rs
+++ src/libksupport/src/kernel/rtio_acp.rs
@@ -1,7 +1,7 @@
 use core::sync::atomic::{Ordering, fence};
 
 use cslice::CSlice;
-use libcortex_a9::{asm, cache};
+use libcortex_a9::asm;
 use vcell::VolatileCell;
 
 #[cfg(has_drtio)]
@@ -17,6 +17,9 @@ pub const RTIO_I_STATUS_OVERFLOW: i32 = 2;
 pub const RTIO_I_STATUS_WAIT_STATUS: i32 = 4; // TODO
 pub const RTIO_I_STATUS_DESTINATION_UNREACHABLE: i32 = 8;
 
+const RTIO_CMD_OUTPUT = 0;
+const RTIO_CMD_INPUT = 1;
+
 #[repr(C)]
 pub struct TimestampedData {
     timestamp: i64,
@@ -25,6 +28,7 @@ pub struct TimestampedData {
 
 #[repr(C, align(64))]
 struct Transaction {
+    /* DOUT */
     request_cmd: i8,
     data_width: i8,
     padding0: [i8; 2],
@@ -32,6 +36,7 @@ struct Transaction {
     request_timestamp: i64,
     request_data: [i32; 16],
     padding1: [i64; 2],
+    /* DIN */
     reply_status: VolatileCell<i32>,
     reply_data: VolatileCell<i32>,
     reply_timestamp: VolatileCell<i64>,
@@ -39,11 +44,13 @@ struct Transaction {
 }
 
 static mut TRANSACTION_BUFFER: Transaction = Transaction {
+    /* DOUT */
     request_cmd: 0,
     data_width: 0,
     request_target: 0,
     request_timestamp: 0,
     request_data: [0; 16],
+    /* DIN */
     reply_status: VolatileCell::new(0),
     reply_data: VolatileCell::new(0),
     reply_timestamp: VolatileCell::new(0),
@@ -118,12 +125,16 @@ unsafe fn process_exceptional_status(channel: i32, status: i32) {
 
 fn await_reply_status() -> i32 {
     unsafe {
+        // dmb and send event (commit the event to gateware)
         fence(Ordering::SeqCst);
         asm::sev();
+        // actually await status
         loop {
             let status = TRANSACTION_BUFFER.reply_status.get();
             if status != 0 {
-                return status
+                // Clear status so we can observe response on the next call
+                TRANSACTION_BUFFER.reply_status.set(0);
+                return status;
             }
         }
     }
@@ -131,16 +142,13 @@ fn await_reply_status() -> i32 {
 
 pub extern "C" fn output(target: i32, data: i32) {
     unsafe {
-        // Clear status so we can observe response
-        TRANSACTION_BUFFER.reply_status.set(0);
-
-        TRANSACTION_BUFFER.request_cmd = 0;
+        TRANSACTION_BUFFER.request_cmd = RTIO_CMD_OUTPUT;
         TRANSACTION_BUFFER.data_width = 1;
         TRANSACTION_BUFFER.request_target = target;
         TRANSACTION_BUFFER.request_timestamp = NOW;
         TRANSACTION_BUFFER.request_data[0] = data;
 
-        let status = await_reply_status() & !0x10000;
+        let status = await_reply_status() & !(1 << 16);
         if status != 0 {
             process_exceptional_status(target >> 8, status);
         }
@@ -149,28 +157,43 @@ pub extern "C" fn output(target: i32, data: i32) {
 
 pub extern "C" fn output_wide(target: i32, data: CSlice<i32>) {
     unsafe {
-        // Clear status so we can observe response
-        TRANSACTION_BUFFER.reply_status.set(0);
-
-        TRANSACTION_BUFFER.request_cmd = 0;
+        TRANSACTION_BUFFER.request_cmd = RTIO_CMD_OUTPUT;
         TRANSACTION_BUFFER.data_width = data.len() as i8;
         TRANSACTION_BUFFER.request_target = target;
         TRANSACTION_BUFFER.request_timestamp = NOW;
         TRANSACTION_BUFFER.request_data[..data.len()].copy_from_slice(data.as_ref());
 
-        let status = await_reply_status() & !0x10000;
+        let status = await_reply_status() & !(1 << 16);
         if status != 0 {
             process_exceptional_status(target >> 8, status);
         }
     }
 }
 
+fn process_exceptional_input_status(status: i32, channel: i32) {
+    if status & RTIO_I_STATUS_OVERFLOW != 0 {
+        artiq_raise!(
+            "RTIOOverflow",
+            "RTIO input overflow on channel {rtio_channel_info:0}",
+            channel as i64,
+            0,
+            0
+        );
+    }
+    if status & RTIO_I_STATUS_DESTINATION_UNREACHABLE != 0 {
+        artiq_raise!(
+            "RTIODestinationUnreachable",
+            "RTIO destination unreachable, input, on channel {rtio_channel_info:0}",
+            channel as i64,
+            0,
+            0
+        );
+    }
+}
+
 pub extern "C" fn input_timestamp(timeout: i64, channel: i32) -> i64 {
     unsafe {
-        // Clear status so we can observe response
-        TRANSACTION_BUFFER.reply_status.set(0);
-
-        TRANSACTION_BUFFER.request_cmd = 1;
+        TRANSACTION_BUFFER.request_cmd = RTIO_CMD_INPUT;
         TRANSACTION_BUFFER.request_timestamp = timeout;
         TRANSACTION_BUFFER.request_target = channel << 8;
         TRANSACTION_BUFFER.data_width = 0;
@@ -204,9 +227,7 @@ pub extern "C" fn input_timestamp(timeout: i64, channel: i32) -> i64 {
 
 pub extern "C" fn input_data(channel: i32) -> i32 {
     unsafe {
-        TRANSACTION_BUFFER.reply_status.set(0);
-
-        TRANSACTION_BUFFER.request_cmd = 1;
+        TRANSACTION_BUFFER.request_cmd = RTIO_CMD_INPUT;
         TRANSACTION_BUFFER.request_timestamp = -1;
         TRANSACTION_BUFFER.request_target = channel << 8;
         TRANSACTION_BUFFER.data_width = 0;
@@ -219,32 +240,9 @@ pub extern "C" fn input_data(channel: i32) -> i32 {
     }
 }
 
-fn process_exceptional_input_status(status: i32, channel: i32) {
-        if status & RTIO_I_STATUS_OVERFLOW != 0 {
-            artiq_raise!(
-                "RTIOOverflow",
-                "RTIO input overflow on channel {rtio_channel_info:0}",
-                channel as i64,
-                0,
-                0
-            );
-        }
-        if status & RTIO_I_STATUS_DESTINATION_UNREACHABLE != 0 {
-            artiq_raise!(
-                "RTIODestinationUnreachable",
-                "RTIO destination unreachable, input, on channel {rtio_channel_info:0}",
-                channel as i64,
-                0,
-                0
-            );
-    }
-}
-
 pub extern "C" fn input_timestamped_data(timeout: i64, channel: i32) -> TimestampedData {
     unsafe {
-        TRANSACTION_BUFFER.reply_status.set(0);
-
-        TRANSACTION_BUFFER.request_cmd = 1;
+        TRANSACTION_BUFFER.request_cmd = RTIO_CMD_INPUT;
         TRANSACTION_BUFFER.request_timestamp = timeout;
         TRANSACTION_BUFFER.request_target = channel << 8;
         TRANSACTION_BUFFER.data_width = 0;
